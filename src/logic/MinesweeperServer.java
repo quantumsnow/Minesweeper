b1e2ca6ec44bb497495729377387ac8ef26b6742
Jakob Scharmberg <jakobtheai@gmail.com>
2016-02-29 20:47:16 +0100

    Initial commit

---
+++ src/logic/MinesweeperServer.java
@@ -0,0 +1,30 @@
+package logic;
+
+import lib.Server;
+
+public class MinesweeperServer extends Server {
+
+	public MinesweeperServer(int port) {
+		super(port);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void processNewConnection(String pClientIP, int pClientPort) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void processMessage(String pClientIP, int pClientPort, String pMessage) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void processClosedConnection(String pClientIP, int pClientPort) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
