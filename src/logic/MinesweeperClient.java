fb07a0d44839ba5c000d3552cb5c1c26e5c18ab5
Jakob Scharmberg <jakobtheai@gmail.com>
2016-03-01 13:43:07 +0100

    Added: first work for server

---
+++ src/logic/MinesweeperClient.java
@@ -0,0 +1,18 @@
+package logic;
+
+import lib.Client;
+
+public class MinesweeperClient extends Client {
+
+	public MinesweeperClient(String ip, int port) {
+		super(ip, port);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void processMessage(String pMessage) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- src/logic/MinesweeperServer.java
+++ src/logic/MinesweeperServer.java
@@ -1,30 +1,68 @@
 package logic;
 
+import lib.List;
 import lib.Server;
 
 public class MinesweeperServer extends Server {
+	private class Player {
+		private String nick, ip;
+		private int port;
+		
+		public Player(String ip, int port) {
+			super();
+			this.ip = ip;
+			this.port = port;
+		}
+
+		public String getNick() {
+			return nick;
+		}
+
+		public String getIp() {
+			return ip;
+		}
+
+		public int getPort() {
+			return port;
+		}
+		
+		void register(String nick) {
+			this.nick = nick;
+		}
+		
+		boolean isRegistered() {
+			return nick != null;
+		}
+	}
+	
+	private List players;
 
 	public MinesweeperServer(int port) {
 		super(port);
-		// TODO Auto-generated constructor stub
+		players = new List();
 	}
 
 	@Override
-	public void processNewConnection(String pClientIP, int pClientPort) {
+	public void processNewConnection(String ip, int port) {
+		players.append(new Player(ip, port));
+	}
+
+	@Override
+	public void processMessage(String ip, int port, String message) {
 		// TODO Auto-generated method stub
 
 	}
 
 	@Override
-	public void processMessage(String pClientIP, int pClientPort, String pMessage) {
-		// TODO Auto-generated method stub
-
+	public void processClosedConnection(String ip, int port) {
+		Player currentPlayer;
+		players.toFirst();
+		while (players.hasAccess()) {
+			currentPlayer = (Player) players.getObject();
+			if (currentPlayer.getIp().equals(ip) && currentPlayer.getPort() == port) {
+				players.remove();
+				return;
+			}
+		}
 	}
-
-	@Override
-	public void processClosedConnection(String pClientIP, int pClientPort) {
-		// TODO Auto-generated method stub
-
-	}
-
 }
